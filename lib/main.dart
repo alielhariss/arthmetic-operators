fcc481b5325ad23d05c301520d517709387c6a5f
alielhariss <drali19900@gamil.com>
2025-09-15 22:29:01 +0300

    add arthmetic operators value to the content

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  int a = 10;
+  int b = 5;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  // % means The remainder of the division
+  // in that example 10 % 5 = 0 because 10 / 5 = 2 so there is no remainder
+  print(a % b);
+  int c = 10;
+  int d = 3;
+  print(c % d);
+  // in that example 10 % 3 = 1 because 10 / 3 = 3 will give you a decimal number so we will try 9 / 3 = 3 so the reminder will equal 1
+}
